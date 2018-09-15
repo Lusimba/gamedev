1da9bd54c6ea3a982b618f1eb905d82fa7d5b2b7
Lusimba <newville92@gmail.com>
2018-09-15 18:47:26 +0530

    updating the files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
-import './App.css';
+
+import 'tachyons';
 
 class App extends Component {
   render() {
+    
     const scene = new THREE.Scene();
     const camera= new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    camera.position.z=5;
     const renderer= new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    //updating viewport
+    window.addEventListener('resize', ()=>{
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width/height;
+      camera.updateProjectionMatrix();
+    });
     // Game Logic
     const update=(x)=>{
-
+      cube.rotation.x +=0.01;
+      cube.rotation.y += 0.005;
     }
+    //controls
+    
+    // create a cube
+    const geometry= new THREE.BoxGeometry(1, 1, 1);
+    const material= new THREE.MeshBasicMaterial({color:0xffff00, wireframe: false});
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    
 
     //Draw Scene
     const render=()=>{
@@ -28,7 +48,7 @@ class App extends Component {
     GameLoop();
 
     return (
-      <h1 ></h1>
+      <h1 className="tc">Rotating Cube</h1>
       
     );
   }
